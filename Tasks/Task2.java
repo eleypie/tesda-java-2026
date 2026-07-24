9be426cde0817b56ad3afc34a0031397a3874717
yelxoxo <clorielann@gmail.com>
2026-07-24 17:14:56 +0800

    Add Task2 primitive concatenation exercise

---
+++ Tasks/Task2.java
@@ -0,0 +1,15 @@
+package Tasks;
+
+public class Task2 {
+  public static void main(String[] args) {
+    char charVar = 'w';
+    int intVar = 3110;
+    byte byteVar = 0;
+    short shortVar = 1;
+    float floatVar = 2.0f;
+    boolean bolVar = true;
+
+    System.out.println("H" + intVar + " " + charVar + byteVar + "r" + shortVar + "d " + floatVar + " " + bolVar);
+
+  }
+}
--- chapter2/LoopLesson.java
+++ chapter2/LoopLesson.java
@@ -1,5 +1,7 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class LoopLesson {
   static boolean op() {
     return true;
@@ -25,12 +27,50 @@ public class LoopLesson {
       // for (;; ) infinite loop
       System.out.println("b = " + b++);
 
-    int b = 0;
-    boolean bol = true;
-    for (; bol;)
-      System.out.println("b = " + b++);
-    for (System.out.println("Initialization"); b < 10; System.out.println("update statement"))
-      System.out.println("b = " + b++);
+    /*
+     * int b = 0;
+     * boolean bol = true;
+     * for (; bol;)
+     * System.out.println("b = " + b++);
+     * for (System.out.println("Initialization"); b < 10;
+     * System.out.println("update statement"))
+     * System.out.println("b = " + b++);
+     */
 
+    System.out.println("enter a valid number");
+    Scanner q = new Scanner(System.in);
+    int limit = q.nextInt();
+    for (int x = 1; x < limit; x++) {
+      for (int y = 1; y <= x; y++) {
+        System.out.print(x);
+      }
+      System.out.println();
+
+    }
+
+    String[] colors = { "black", "red", "pink", "yellow", "white", "blue" };
+    for (String color : colors)
+      System.out.println("Color: " + color);
+
+    /*
+     * ROW: for (int p = 1; p <= 10; p++) {
+     * COL: for (int l = 1; l <= 10; l++) {
+     * System.out.print(l * p + "\t");
+     * }
+     * System.out.println();
+     * 
+     * }
+     */
+
+    ROW_TABLE: for (int p = 1; p <= 10; p++) {
+      COL_TABLE: for (int l = 1; l <= 10; l++) {
+        if (l == 5)
+          // break ROW_TABLE;
+          continue ROW_TABLE;
+        System.out.print(l * p + "\t");
+      }
+      System.out.println();
+
+    }
   }
 }
